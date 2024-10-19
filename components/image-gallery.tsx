30a931622ff10436e5a2029a931c5bf6b4774847
Sevda Anefi <sevda.m.anefi@gmail.com>
2024-10-19 22:28:33 +0300

    switch to Image/next and mimicked mosaic grid

--- components/image-gallery.tsx
+++ components/image-gallery.tsx
@@ -4,6 +4,7 @@ import { AiImageType } from "@/db/schema"
 import { downloadAiImage } from "@/lib/helpers"
 import { Heart, Trash2, Download } from "lucide-react"
 import Link from "next/link"
+import Image from "next/image"
 
 
 interface ImageGalleryProps {
@@ -12,11 +13,24 @@ interface ImageGalleryProps {
 
 export function ImageGallery({ images }: ImageGalleryProps) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {images.map(({url, name, id, isFavorite, description = "No description available", title = "No title available"}) => (
-        <div key={id} className="relative group">
-          <Link href={`/img/${id}`}>
-            <img src={url} alt={`Generated image ${id} - ${name} `} className="w-full h-auto rounded-lg" />
+    <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
+      {images.map(({ url, name, id, isFavorite, description = "No description available", title = "No title available", blurDataUrl }) => (
+        <div key={id} className="relative group mb-5">
+          <Link href={`/img/${id}`} className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight">
+            <Image
+              alt={`Generated image ${id} - ${name}`}
+              className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+              style={{ transform: "translate3d(0, 0, 0)" }}
+              placeholder={blurDataUrl ? "blur" : "empty"}
+              blurDataURL={blurDataUrl || undefined}
+              src={url}
+              width={720}
+              height={480}
+              sizes="(max-width: 640px) 100vw,
+                (max-width: 1280px) 50vw,
+                (max-width: 1536px) 33vw,
+                25vw"
+            />
             <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end p-4 rounded-lg">
               <h3 className="text-white font-semibold text-lg mb-1">{title}</h3>
               <p className="text-white text-sm">{description}</p>
@@ -29,7 +43,7 @@ export function ImageGallery({ images }: ImageGalleryProps) {
             <Button variant="secondary" size="icon" onClick={() => deleteAiImage(id)}>
               <Trash2 className="h-4 w-4" />
             </Button>
-            <Button variant="secondary" size="icon" onClick={() =>  downloadAiImage(url, name)}>
+            <Button variant="secondary" size="icon" onClick={() => downloadAiImage(url, name)}>
               <Download className="h-4 w-4" />
             </Button>
           </div>
@@ -38,4 +52,3 @@ export function ImageGallery({ images }: ImageGalleryProps) {
     </div>
   )
 }
- 
